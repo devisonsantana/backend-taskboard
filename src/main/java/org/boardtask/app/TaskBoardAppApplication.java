7881be14f6be53860ec5469018845684d7b2c06c
DevisonSantana <devisonsantana@hotmail.com>
2025-03-30 14:46:09 -0300

    initial commit -- task board manager api

---
+++ src/main/java/org/boardtask/app/TaskBoardAppApplication.java
@@ -0,0 +1,13 @@
+package org.boardtask.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBoardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBoardAppApplication.class, args);
+	}
+
+}
